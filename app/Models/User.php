371ece8c196c971be2fc520dc8e19a3150eedb97
fanboykun <irfanramadhan1812@gmail.com>
2021-12-03 22:29:57 +0700

    remove spatie permission

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -11,7 +10,6 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasRoles;
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
